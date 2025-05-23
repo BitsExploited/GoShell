d2dc7a00e52bd3af9cfd4af640b2a8f65aaf4434
BitsExploited <tspamiitesh@gmail.com>
2025-05-23 07:27:33 +0530

    Exit command

--- app/main.go
+++ app/main.go
@@ -1,34 +1,47 @@
-package main
-
 import (
+
 	"bufio"
+
 	"fmt"
+
 	"os"
+
 	"strings"
+
 )
 
 // Ensures gofmt doesn't remove the "fmt" import in stage 1 (feel free to remove this!)
+
 var _ = fmt.Fprint
 
 func main() {
+
 	for {
+
 		fmt.Fprint(os.Stdout, "$ ")
 
 		command, err := bufio.NewReader(os.Stdin).ReadString('\n')
+
 		if err != nil {
-			fmt.Fprintln(os.Stderr, "Error reading input:", err)
+
+			fmt.Fprint(os.Stderr, "Error reading input: ", err)
+
 			os.Exit(1)
+
 		}
 
 		command = strings.TrimSuffix(command, "\n")
 
-		if command == "exit" {
-			if command == "exit 0" {
-				os.Exit(0)
-			} else {
-				fmt.Printf("%s: command not found\n", command)
-			}
+		if command == "exit 0" {
+
+			break
+
+		} else {
+
+			fmt.Printf("%s: command not found\n", command)
+
 		}
 
 	}
+
 }
