e698e706f2d7500276fba0184c474c068efea715
BitsExploited <tspamiitesh@gmail.com>
2025-05-23 07:17:41 +0530

    Infinite Loop

--- app/main.go
+++ app/main.go
@@ -10,18 +10,25 @@ import (
 var _ = fmt.Fprint
 
 func main() {
-	 fmt.Fprint(os.Stdout, "$ ")
-	
+	fmt.Fprint(os.Stdout, "$ ")
+
 	command, err := bufio.NewReader(os.Stdin).ReadString('\n')
 	if err != nil {
-
 		fmt.Fprintln(os.Stderr, "Error reading input:", err)
-
 		os.Exit(1)
-
 	}
 
 	fmt.Println(command[:len(command)-1] + ": command not found")
-	// Wait for user input
-	bufio.NewReader(os.Stdin).ReadString('\n')
+
+	for {
+		fmt.Fprint(os.Stdout, "$ ")
+
+		command, err := bufio.NewReader(os.Stdin).ReadString('\n')
+		if err != nil {
+			fmt.Fprintln(os.Stderr, "Error reading input:", err)
+			os.Exit(1)
+		}
+
+		fmt.Println(command[:len(command)-1] + ": command not found")
+	}
 }
