bc8f0f1d32ce6d666b14e4cbfa7d5034b495dda1
BitsExploited <tspamiitesh@gmail.com>
2025-05-23 10:11:58 +0530

    executable

--- app/main.go
+++ app/main.go
@@ -5,11 +5,34 @@ import (
 	"fmt"
 	"os"
 	"os/exec"
+	"path/filepath"
 	"strings"
 )
 
 // Ensures gofmt doesn't remove the "fmt" import in stage 1 (feel free to remove this!)
 var _ = fmt.Fprint
+
+func findExecutable(command string, paths []string) string {
+
+	for _, path := range paths {
+
+		filePath := filepath.Join(path, command)
+
+		fileInfo, err := os.Stat(filePath)
+
+		if err == nil && fileInfo.Mode().Perm()&0111 != 0 {
+
+			// Check if file exists and is executable
+
+			return filePath
+
+		}
+
+	}
+
+	return ""
+
+}
 
 func isBuiltIn(command string) bool {
 	builtIns := []string{"exit", "echo", "type"}
@@ -38,13 +61,14 @@ func typeCommand(argument string, path string) {
 	} else {
 		fmt.Printf("%s: not found\n", argument)
 	}
+	return
 }
 
 func main() {
 	for {
 		fmt.Fprint(os.Stdout, "$ ")
 		command, _ := bufio.NewReader(os.Stdin).ReadString('\n')
-		
+
 		command = strings.TrimSpace(command)
 		commandParts := strings.Fields(command)
 
@@ -62,7 +86,16 @@ func main() {
 			typeCommand(args[0], path)
 
 		} else {
-			fmt.Printf("%s: command not found\n", command)
+			filepath := findExecutable(args[0], strings.Split(os.Getenv("PATH"), ":"))
+			if filepath != "" {
+				cmd := exec.Command(command, args...)
+				cmd.Stdout = os.Stdout
+				cmd.Stderr = os.Stderr
+				cmd.Run()
+			} else {
+
+				fmt.Printf("%s: command not found\n", command)
+			}
 		}
 	}
 }
