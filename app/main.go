7487fceead2a35d13c4a3fc5f7bf23cf98cd4db8
BitsExploited <tspamiitesh@gmail.com>
2025-05-23 07:35:54 +0530

    echo command

--- app/main.go
+++ app/main.go
@@ -1,49 +1,32 @@
 package main
 
 import (
-
 	"bufio"
-
 	"fmt"
-
 	"os"
-
 	"strings"
-
 )
 
 // Ensures gofmt doesn't remove the "fmt" import in stage 1 (feel free to remove this!)
-
 var _ = fmt.Fprint
 
 func main() {
-
 	for {
-
 		fmt.Fprint(os.Stdout, "$ ")
-
 		command, err := bufio.NewReader(os.Stdin).ReadString('\n')
-
 		if err != nil {
-
 			fmt.Fprint(os.Stderr, "Error reading input: ", err)
-
 			os.Exit(1)
-
 		}
-
 		command = strings.TrimSuffix(command, "\n")
-
+		
 		if command == "exit 0" {
-
 			break
-
+		} else if strings.HasPrefix(command, "echo ") {
+			phrase := command[5:]
+			fmt.Println(phrase)
 		} else {
-
 			fmt.Printf("%s: command not found\n", command)
-
 		}
-
 	}
-
 }
